935eea846270276e75364d1562c27f01d3ecb130
Camyul <vankata.lt@outlook.com>
2016-11-18 21:58:25 +0200

    Complete problem 03.03

---
+++ C# Advanced/03. Methods/02. Get largest number/02. Get largest number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static int GetMax(int firstNum, int secondNum)
+    {
+        int maxNum = firstNum;
+        if (maxNum < secondNum)
+        {
+            maxNum = secondNum;
+        }
+        return maxNum;
+    }
+    static void Main()
+    {
+        string[] numbers = Console.ReadLine().Split();
+        int maxNumber = GetMax(int.Parse(numbers[0]), int.Parse(numbers[1]));
+        maxNumber = GetMax(maxNumber, int.Parse(numbers[2]));
+        Console.WriteLine(maxNumber);
+    }
+}
---
+++ C# Advanced/03. Methods/03. English digit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static string GetDigitAsWord(long num)
+    {
+        num = num % 10;
+        string digit = "";
+        switch (num)
+        {
+            case 0: digit = "zero";break;
+            case 1: digit = "one"; break;
+            case 2: digit = "two"; break;
+            case 3: digit = "three"; break;
+            case 4: digit = "four"; break;
+            case 5: digit = "five"; break;
+            case 6: digit = "six"; break;
+            case 7: digit = "seven"; break;
+            case 8: digit = "eight"; break;
+            case 9: digit = "nine"; break;
+            default:
+                break;
+        }
+        return digit;
+    }
+    static void Main()
+    {
+        long number = long.Parse(Console.ReadLine());
+        string word = GetDigitAsWord(number);
+        Console.WriteLine(word);
+    }
+}
