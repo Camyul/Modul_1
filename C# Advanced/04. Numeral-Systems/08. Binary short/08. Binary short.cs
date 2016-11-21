91dd12152b5124dac217fddf36a27ca5d03461a2
Camyul <vankata.lt@outlook.com>
2016-11-21 23:49:25 +0200

    Complete Problem 04.08

--- C# Advanced/04. Numeral-Systems/08. Binary short/08. Binary short.cs
+++ C# Advanced/04. Numeral-Systems/08. Binary short/08. Binary short.cs
@@ -4,19 +4,28 @@ class Program
     static string DecToBin(short shortNum, short numBase)
     {
         string result = "";
-        short shortN = Math.Abs(shortNum);
-        do
+        if (shortNum > 0) 
         {
-            int digit = shortN % numBase;
-            shortN /= 2;
-            result = digit + result;
-        } while (shortN > 0);
-        if (shortNum < 0)
+            do
+            {
+                int digit = shortNum % numBase;
+                shortNum /= 2;
+                result = digit + result;
+            } while (shortNum > 0);
+        } 
+        else if (shortNum < 0)
         {
-            result = (~(int.Parse(result) + 1)).ToString();
+            shortNum = Math.Abs(++shortNum);
+            do
+            {
+                string digit = (shortNum % 2 == 0) ? "1" : "0";
+                shortNum /= numBase;
+                result = digit + result;
+            } while (shortNum > 0);
+            result = result.PadLeft(16, '1');
         }
         return result;
-    }
+    } 
     static void Main()
     {
         short shortNum = short.Parse(Console.ReadLine());
@@ -25,4 +34,20 @@ class Program
         binNum = DecToBin(shortNum, numBase);
         Console.WriteLine(binNum.PadLeft(16, '0'));
     }
+    //static void Main()
+    //{
+    //    short signedShort = short.Parse(Console.ReadLine());
+
+    //    string binary = string.Empty;
+
+    //    for (int i = 0; i < 16; i++)
+    //    {
+    //        string sign = (signedShort & 1) == 0 ? "0" : "1";
+    //        binary = sign + binary;
+
+    //        signedShort >>= 1;
+    //    }
+
+    //    Console.WriteLine(binary.PadLeft(16, '0'));
+    //}
 }
