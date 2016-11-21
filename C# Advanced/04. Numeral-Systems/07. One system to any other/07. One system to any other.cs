f2ef0f627afba43c4fe2f80d24bf5158f7e779a9
Camyul <vankata.lt@outlook.com>
2016-11-21 21:46:47 +0200

    Working on Problem 04.08

---
+++ C# Advanced/04. Numeral-Systems/07. One system to any other/07. One system to any other.cs
@@ -0,0 +1,73 @@
+﻿using System;
+class Program
+{
+    
+    static void Main()
+    {
+        int baseS = int.Parse(Console.ReadLine());
+        string numBaseS = Console.ReadLine();
+        int baseD = int.Parse(Console.ReadLine());
+        string numBaseD = "";
+        if ((baseD == 10) && (baseS < 10))
+        {
+            numBaseD = AnyToDecimal(numBaseS, baseS);
+        }
+        if ((baseD == 10) && (baseS > 10))
+        {
+            numBaseD = AnyUpOfTenToDecimal(numBaseS, baseS);
+        }
+        else if(baseS < 11)
+        {
+            numBaseD = AnyToDecimal(numBaseS, baseS);
+            numBaseD = DecimalToAny(long.Parse(numBaseD), baseD);
+        }
+        else if (baseS > 10)
+        {
+            numBaseD = AnyUpOfTenToDecimal(numBaseS, baseS);
+            numBaseD = DecimalToAny(long.Parse(numBaseD), baseD);
+        }
+        Console.WriteLine(numBaseD);
+    }
+    static string AnyUpOfTenToDecimal(string num, int numBase)
+    {
+        long result = 0;
+        char[] digits = num.ToCharArray();
+        foreach (char digit in digits)
+        {
+            if (char.IsDigit(digit))
+            {
+                result = (result * numBase) + (digit - '0');
+            }
+            else
+            {
+                result = (result * numBase) + (char.ToUpper(digit) - 'A' + 10);
+            }
+
+        }
+        return result.ToString();
+    }
+    static string DecimalToAny(long num, int numBase)
+    {
+        string result = "";
+        string hexDigit = "0123456789ABCDEF";
+        do
+        {
+            long index = num % numBase;
+            char digit = hexDigit[(int)index];
+            result = digit + result;
+            num /= numBase;
+        } while (num > 0);
+
+        return result;
+    }
+    static string AnyToDecimal(string numBaseS, int baseS)
+    {
+        long result = 0;
+        foreach (char bin in numBaseS)
+        {
+            result = (result * baseS) + (bin - '0');
+        }
+        return result.ToString();
+    }
+    
+}
---
+++ C# Advanced/04. Numeral-Systems/08. Binary short/08. Binary short.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static string DecToBin(short shortNum, short numBase)
+    {
+        string result = "";
+        short shortN = Math.Abs(shortNum);
+        do
+        {
+            int digit = shortN % numBase;
+            shortN /= 2;
+            result = digit + result;
+        } while (shortN > 0);
+        if (shortNum < 0)
+        {
+            result = (~(int.Parse(result) + 1)).ToString();
+        }
+        return result;
+    }
+    static void Main()
+    {
+        short shortNum = short.Parse(Console.ReadLine());
+        short numBase = 2;
+        string binNum = "";
+        binNum = DecToBin(shortNum, numBase);
+        Console.WriteLine(binNum.PadLeft(16, '0'));
+    }
+}
