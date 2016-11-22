d951dc99bcfa4b809ba3716728e4350a6045381f
Camyul <vankata.lt@outlook.com>
2016-11-22 10:04:23 +0200

    Working on Problem 01.05

---
+++ C# Advanced/01. Multidimensional-Arrays/04. Binary search/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+    static int BinSearch(int[] arr,int maxIndex,int minIndex, int k)
+    {
+       
+        int tempIndex = (maxIndex - minIndex) / 2;
+        while ((arr[maxIndex] <= k) && (arr[maxIndex - 1] > k))
+        {
+            if (k < arr[tempIndex])
+            {
+                maxIndex = tempIndex;
+            }
+            else if (k >= arr[tempIndex])
+            {
+                minIndex = tempIndex;
+            }
+        }
+        return maxIndex;
+    }
+    static void Main()
+    {
+        string[] strArr = Console.ReadLine().Split(' ');
+        int k = int.Parse(Console.ReadLine());
+        int[] arr = new int[strArr.Length];
+        int maxIndex = arr.Length - 1;
+        int minIndex = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = int.Parse(strArr[i]);
+        }
+        Array.Sort(arr);
+        maxIndex = BinSearch(arr, maxIndex, minIndex, k);
+        Console.WriteLine(maxIndex);
+    }
+}
---
+++ C# Advanced/01. Multidimensional-Arrays/5. Sort by string length/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class Program
+{
+    static List<string> SortList(List<string> strList)
+    {
+        int count = 0;
+        while (count < strList.Count)
+        {
+            
+            int minIndex = FindMinElement(strList, strList.Count, strList[count].Length);
+            count++;
+            string temp = strList[0];
+            strList[0] = strList[minIndex];
+            strList[minIndex] = temp;
+        }
+        return strList;
+    }
+    static int FindMinElement(List<string> strList, int maxCount, int element)
+    {
+        int minIndex = int.MaxValue;
+        for (int i = 0; i < maxCount; i++)
+        {
+            if (strList[i].Length < element)
+            {
+                minIndex = i;
+                element = strList[i].Length;
+            }
+        }
+        return minIndex;
+    }
+    static void Main()
+    {
+        List<string> strList = new List<string>();
+        string sequence = "";
+        while (sequence != "end")
+        {
+            sequence = Console.ReadLine();
+            strList.Add(sequence);
+        }
+        SortList(strList);
+        Console.WriteLine(string.Join(" ", strList));
+    }
+}
