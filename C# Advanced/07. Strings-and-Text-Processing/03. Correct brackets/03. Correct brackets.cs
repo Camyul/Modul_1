c659496ce2936358ee0f250031509b7b518618d9
Camyul <vankata.lt@outlook.com>
2016-11-25 22:16:16 +0200

    Complete Problem 07.04

--- C# Advanced/07. Strings-and-Text-Processing/03. Correct brackets/03. Correct brackets.cs
+++ C# Advanced/07. Strings-and-Text-Processing/03. Correct brackets/03. Correct brackets.cs
@@ -1,15 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Correct_brackets
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     class Program
     {
+        private static bool CorrectBrackets(string expression)
+        {
+            bool isEqual = false;
+            int leftBracket = 0;
+            int rightBracket = 0;
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    leftBracket++;
+                }
+                else if (expression[i] == ')')
+                {
+                    rightBracket++;
+                }
+            }
+            if (leftBracket == rightBracket)
+            {
+                isEqual = true;
+            }
+            return isEqual;
+        }
         static void Main()
         {
+            string expression = Console.ReadLine();
+            bool isCorrect = CorrectBrackets(expression);
+            if (!isCorrect)
+            {
+                Console.WriteLine("Incorrect");
+            }
+            else
+            {
+                int indexLeft = expression.IndexOf('(');
+                int indexRight = expression.IndexOf(')');
+                while (indexLeft != -1 && indexRight != -1)
+                {
+                    
+                    if (indexLeft < indexRight)
+                    {
+                        isCorrect = true;
+                        
+                    }
+                    else
+                    {
+                        isCorrect = false;
+                        break;
+                    }
+                    indexLeft = expression.IndexOf('(', indexLeft + 1);
+                    indexRight = expression.IndexOf(')', indexRight) + 1;
+                }
+                string result = isCorrect ? "Correct" : "Incorrect";
+                Console.WriteLine(result);
+            }
         }
+
     }
 }
---
+++ C# Advanced/07. Strings-and-Text-Processing/04. Sub-string in text/04. Sub-string in text.cs
@@ -0,0 +1,55 @@
+﻿namespace Sub_string_in_text
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    class Program
+    {
+        static void Main()
+        {
+            string pattern = Console.ReadLine().ToLower();
+            string text = Console.ReadLine().ToLower();
+            int index = 0;
+            int count = 0;
+            do
+            {
+                index = text.IndexOf(pattern, index + 1);
+                if (index != -1)
+                {
+                    count++;
+                }
+
+            } while (index != -1);
+            Console.WriteLine(count);
+        }
+    }
+}
+
+//namespace Sub_string_in_text
+//{
+//    using System;
+//    using System.Collections.Generic;
+//    using System.Linq;
+//    using System.Text;
+//    using System.Threading.Tasks;
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            string pattern = Console.ReadLine().ToLower();
+//            string text = Console.ReadLine().ToLower();
+//            int count = 0;
+//            for (int i = 0; i < (text.Length - pattern.Length + 1); i++)
+//            {
+//                if (text.Substring(i, pattern.Length) == pattern)
+//                {
+//                    count++;
+//                }
+
+//            }
+//            Console.WriteLine(count);
+//        }
+//    }
+//}
