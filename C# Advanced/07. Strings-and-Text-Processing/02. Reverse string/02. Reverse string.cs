888c163576ab0cb94c506eaa281529c78a6b2546
Camyul <vankata.lt@outlook.com>
2016-11-25 09:12:13 +0200

    Complete Problem 07.01

---
+++ C# Advanced/07. Strings-and-Text-Processing/02. Reverse string/02. Reverse string.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_string
+{
+    class Program
+    {
+        public static string Revers(string strFrase)
+        {
+            char[] charArr = strFrase.ToCharArray();
+            Array.Reverse(charArr);
+            return new string(charArr);
+        }
+        static void Main()
+        {
+            string frase = Console.ReadLine();
+            frase = Revers(frase);
+            Console.WriteLine(frase);
+        }
+    }
+}
---
+++ C# Advanced/07. Strings-and-Text-Processing/03. Correct brackets/03. Correct brackets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Correct_brackets
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
