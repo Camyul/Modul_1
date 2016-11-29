dbadaab1b45c155d371a9e4f5220db714be0bbc3
Camyul <vankata.lt@outlook.com>
2016-11-29 22:07:28 +0200

    Complete Problem 08.02

---
+++ C# Advanced/08. Exception-Handling/01. Square root/01. Square root.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace _01.Square_root
+{
+    class SquareRoot
+    {
+       
+        static void Main()
+        {
+            
+            try
+            {
+                double num = double.Parse(Console.ReadLine());
+                num = SqRoot(num);
+                Console.WriteLine("{0:F3}", num);
+            }
+            catch (SystemException ann)
+            {
+                Console.WriteLine("Invalid number");
+                //Console.WriteLine(ann.Message);
+                
+            }
+            
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+            
+        }
+
+        private static double SqRoot(double num)
+        {
+            if (num <= 0)
+            {
+                throw new SystemException();
+            }
+            num = Math.Sqrt(num);
+            return num;
+        }
+    }
+}
---
+++ C# Advanced/08. Exception-Handling/02. Enter numbers/02. Enter numbers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _02.Enter_numbers
+{
+    class EnterNumbers
+    {
+        static int[] numb = new int[10];
+        static int[] ReadNumbers(int start, int end)
+        {
+            for (int i = start; i < end; i++)
+            {
+                numb[i] = int.Parse(Console.ReadLine());
+                if (numb[i] <= 1 || numb[i] > 99)
+                {
+                    throw new Exception();
+                }
+                
+            }
+            return numb;
+        }
+        static void Main()
+        {
+            
+            
+            try
+            {
+                numb = ReadNumbers(0, 10);
+                CheckSorted(0, 10);
+                Console.WriteLine("1 < " + string.Join(" < ", numb) + " < 100");
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Exception");
+            }
+        }
+
+        private static void CheckSorted(int start, int end)
+        {
+            for (int i = start; i < end - 1; i++)
+            {
+                if (numb[i] >= numb[i + 1])
+                {
+                    throw new Exception();
+                }
+            }
+        }
+    }
+}
