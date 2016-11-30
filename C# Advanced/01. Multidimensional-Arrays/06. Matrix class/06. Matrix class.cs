90624a44e3bad565b31b49c7492cb9d68e27e38b
Camyul <vankata.lt@outlook.com>
2016-11-30 15:27:57 +0200

    Working on Problem 01.06

---
+++ C# Advanced/01. Multidimensional-Arrays/06. Matrix class/06. Matrix class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Matrix_namespace;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix_class
+{   
+    class MatrixClass
+    {
+        static void Main()
+        {
+            Matrix firstMatrix = new Matrix();
+
+        }
+    }
+}
---
+++ C# Advanced/09. Text-Files/01. Odd lines/01. Odd lines.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace _01.Odd_lines
+{
+    class OddLines
+    {
+      
+        static void Main()
+        {
+            string pathToFile = @"../../oddLines.txt";
+            string streamLine = "";
+            StreamReader oddLine = new StreamReader(pathToFile, Encoding.UTF8);
+            Console.OutputEncoding = Encoding.UTF8;
+            using (oddLine)
+            {
+                
+                int i = 0;
+                while( streamLine != null)
+                {
+                    streamLine = oddLine.ReadLine();
+                    if (i % 2 == 1)
+                    {
+                        Console.WriteLine(streamLine);
+                    }
+                    i++;
+                }
+            }
+        }
+    }
+}
