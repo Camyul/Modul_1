a4b2bfaa9fe11f0f10cd9ef2ceeba5b8c730a281
Camyul <vankata.lt@outlook.com>
2016-12-01 09:45:01 +0200

    cOMPLETE pROBLEM 03.12

---
+++ C# Advanced/03. Methods/12. Subtracting polynomials/12. Subtracting polynomials.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Subtracting_polynomials
+{
+    class SubtractingPolynomials
+    {
+
+        static int[] ConvertArray(string[] strArr, int[] intArr)
+        {
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                intArr[i] = int.Parse(strArr[i]);
+            }
+            return intArr;
+        }
+        static void SumArray(int[] firstArr, int[] secondArr, int[] sumArr)
+        {
+            for (int i = 0; i < firstArr.Length; i++)
+            {
+                sumArr[i] = firstArr[i] + secondArr[i];
+            }
+            return;
+        }
+        static void SubstractionArray(int[] firstArr, int[] secondArr, int[] substractionArr)
+        {
+            for (int i = 0; i < firstArr.Length; i++)
+            {
+                substractionArr[i] = firstArr[i] - secondArr[i];
+            }
+            return;
+        }
+        static void MultipliArray(int[] firstArr, int[] secondArr, int[] resultArr)
+        {
+            for (int i = 0; i < firstArr.Length; i++)
+            {
+                resultArr[i] = firstArr[i] * secondArr[i];
+            }
+            return;
+        }
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            string[] firstArray = Console.ReadLine().Split();
+            string[] secondArray = Console.ReadLine().Split();
+            int[] firstPoly = new int[num];
+            int[] secondPoly = new int[num];
+            int[] resultPoly = new int[num];
+            firstPoly = ConvertArray(firstArray, firstPoly);
+            secondPoly = ConvertArray(secondArray, secondPoly);
+            SumArray(firstPoly, secondPoly, resultPoly);
+            Console.WriteLine(string.Join(" ", resultPoly));
+            SubstractionArray(firstPoly, secondPoly, resultPoly);
+            Console.WriteLine(string.Join(" ", resultPoly));
+            MultipliArray(firstPoly, secondPoly, resultPoly);
+            Console.WriteLine(string.Join(" ", resultPoly));
+        }
+    }
+}
+
---
+++ C# Advanced/03. Methods/15. Number calculations/15. Number calculations.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_calculations
+{
+    class NumberCalculations  // NE E RESHENA - NE RABOTI!!!!!!
+    {
+        static List<dynamic> nums = new List<dynamic>();
+        static void ConvertArr<T>(T strArr)
+        {
+            
+                nums.Add(strArr);
+            
+            return;
+        }
+       
+        static void Main()
+        {
+            string[] elements = Console.ReadLine().Split(' ');
+            for (int i = 0; i < elements.Length; i++)
+            {
+                nums[i] = ConvertArr(elements[i]);
+            }
+            int minElement = MinimalElement(nums);
+            int maxElement = MaximalElement(nums);
+            double average = AverageElements(nums);
+            int sumElements = SumElements(nums);
+            long prodElements = ProductElements(nums);
+            Console.WriteLine("{0}\n{1}\n{2:F2}\n{3}\n{4}",
+                minElement, maxElement, average, sumElements, prodElements);
+        }
+        static long ProductElements(int[] nums)
+        {
+            long product = 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                product *= nums[i];
+            }
+            return product;
+        }
+        static int SumElements(int[] nums)
+        {
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+            return sum;
+        }
+        static double AverageElements(int[] nums)
+        {
+            double avrg = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                avrg += nums[i];
+            }
+            return avrg / nums.Length;
+        }
+        static int MaximalElement(int[] nums)
+        {
+            int max = int.MinValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (max < nums[i])
+                {
+                    max = nums[i];
+                }
+            }
+            return max;
+        }
+        static int MinimalElement(int[] nums)
+        {
+            int min = int.MaxValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (min > nums[i])
+                {
+                    min = nums[i];
+                }
+            }
+            return min;
+        }
+       
+    }
+}
