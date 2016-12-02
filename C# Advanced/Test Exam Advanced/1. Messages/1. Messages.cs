64efb98aa949f32cf48ca2f3b9f5be32cfcda46d
Camyul <vankata.lt@outlook.com>
2016-12-02 02:27:16 +0200

    Complete Test Exam Task1

---
+++ C# Advanced/Test Exam Advanced/1. Messages/1. Messages.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Messages
+{
+    class Messages
+    {
+        static BigInteger Encode(string enWord)
+        {
+            string result = String.Empty;
+            string encodeSecuence = "cadxoznopcykminmarkonivaogiyan";
+            List <string> enArr = new List<string>();
+            for (int i = 0; i < enWord.Length; i += 3)
+            {
+                enArr.Add(enWord.Substring(i, 3));
+            }
+            for (int i = 0; i < enArr.Count; i++)
+            {
+                result += encodeSecuence.IndexOf(enArr[i]) / 3;
+            }
+            return BigInteger.Parse(result);
+        }
+        static void Main()
+        {
+            string encriptedWord = Console.ReadLine();
+            char operation = char.Parse(Console.ReadLine());
+            string secondEncriptedWord = Console.ReadLine();
+            BigInteger firstEncrWord = Encode(encriptedWord);
+            BigInteger secondEncrWord = Encode(secondEncriptedWord);
+            BigInteger result = (operation == '+') ? (firstEncrWord + secondEncrWord) : (firstEncrWord - secondEncrWord);
+            string decriptedResult = Decode(result);
+
+            Console.WriteLine(decriptedResult);
+        }
+
+        private static string Decode(BigInteger result)
+        {
+            string decodeSecuence = "cadxoznopcykminmarkonivaogiyan";
+            string decodeResult = string.Empty;
+            while(result > 0)
+            {
+                BigInteger digit = result % 10;
+                result /= 10;
+                decodeResult = decodeSecuence.Substring((int)digit * 3, 3) + decodeResult;
+            }
+            return decodeResult;
+        }
+    }
+}
