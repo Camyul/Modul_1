b360b4e5b8ff0b56baa931cf4ad2e42635a47df1
Camyul <vankata.lt@outlook.com>
2016-12-03 02:11:58 +0200

    Complete Problem 07.16

---
+++ C# Advanced/07. Strings-and-Text-Processing/14. Word dictionary/14. Word dictionary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word_dictionary
+{
+    class WordDictionary
+    {
+        static void Main()
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add(".NET", "platform for applications from Microsoft");
+            dictionary.Add("CLR", "managed execution environment for .NET");
+            dictionary.Add("namespace", "hierarchical organization of classes");
+
+            Console.Write("Please insert a word(\".NET\", \"CLR\", \"namespace\"): ");
+            string word = Console.ReadLine();
+            if (dictionary.ContainsKey(word))
+            {
+                Console.WriteLine("{0} is: {1}", word, dictionary[word]);
+            }
+            else
+            {
+                Console.WriteLine("Dictionary don't contain this word!");
+            }
+                        
+        }
+    }
+}
---
+++ C# Advanced/07. Strings-and-Text-Processing/16. Date difference/16. Date difference.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Date_difference
+{
+    class DateDifference
+    {
+        static void Main()
+        {
+            Console.Write("Enter First Date (dd/MM/yyyy): ");
+            DateTime firstDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Enter Second Date (dd/MM/yyyy): ");
+            DateTime secondDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+            int count = 0;
+            while (firstDate != secondDate)
+            {
+               firstDate = firstDate.AddDays(1);
+                count++;
+            }
+            Console.WriteLine("{0} days",count);
+
+        }
+    }
+}
