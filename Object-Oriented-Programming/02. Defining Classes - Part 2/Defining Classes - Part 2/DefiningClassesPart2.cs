675b7e1eca81e7de016e9caa97c05ac9e3f79dfc
Camyul <vankata.lt@outlook.com>
2016-12-20 20:19:34 +0200

    Complete Task CSharp 02.01

---
+++ Object-Oriented-Programming/02. Defining Classes - Part 2/Defining Classes - Part 2/DefiningClassesPart2.cs
@@ -0,0 +1,14 @@
+﻿namespace Defining_Classes___Part_2
+{
+    using System;
+    using Point3D_ClassLibrary;
+
+    class DefiningClassesPart2
+    {
+        static void Main()
+        {
+            Point3D firstPoint = new Point3D (1.1, 2.2, 3.3);
+            Console.WriteLine(firstPoint.ToString());
+        }
+    }
+}
---
+++ Object-Oriented-Programming/02. Defining Classes - Part 2/Point3D_ClassLibrary/Point3D.cs
@@ -0,0 +1,21 @@
+﻿namespace Point3D_ClassLibrary
+{
+    using System;
+    public struct Point3D
+    {
+        private double x;
+        private double y;
+        private double z;
+
+        public Point3D(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public override string ToString()
+        {
+            return String.Format("Coordinate:\tX\tY\tZ\nValue:\t\t{0}\t{1}\t{2}", this.x, this.y, this.z);
+        }
+    }
+}
