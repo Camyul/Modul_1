41fb597159f12c96aaf60039c0a24a2523679b5b
Camyul <vankata.lt@outlook.com>
2016-12-27 01:32:55 +0200

    Complete CSharp Task 03.02

--- Object-Oriented-Programming/03. Extension-Delegates-Lambda-LINQ/2. IEnumerable extensions/02. IEnumerable extensions.cs
+++ Object-Oriented-Programming/03. Extension-Delegates-Lambda-LINQ/2. IEnumerable extensions/02. IEnumerable extensions.cs
@@ -4,6 +4,8 @@ namespace IEnumerable_extensions
 {
     using System;
     using System.Collections.Generic;
+    using System.Threading;
+    using System.Globalization;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -11,17 +13,31 @@ namespace IEnumerable_extensions
     {
         static void Main()
         {
-            List<int> integers = new List<int> { 1, 2, 3, 4, 5 };
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;  
+
+            List<int> integers = new List<int> { 9, 1, 2, 3, 4, 5 };  //Create List of int 
             Print(integers);
 
+            IEnumerable<double> doubles = new List<double> { 4.5, 1.1, 2.2, 3.3, 4.4, 5.5 };   //Create List of double
+            Print(doubles);
+
+            var decimals = new List<decimal> {12m,  1m, 2m, 31.2m, 4.7m, 5.5m };    //Create List of decimal
+            Print(decimals);
+
         }
 
-        private static void Print(IEnumerable<T> elements)
+        private static void Print<T>(IEnumerable<T> elements)  //Print Results
         {
             Console.WriteLine(string.Join(" ", elements));
-            Console.WriteLine(elements.SumOfElements());
+            Console.WriteLine("Sum: {0:F2}", elements.SumOfElements());
+            Console.WriteLine("Product: {0:F2}", elements.MultiplyElement());
+            Console.WriteLine("Min: {0:F2}", elements.MinElement());
+            Console.WriteLine("Max: {0:F2}", elements.MaxElement());
+            Console.WriteLine("Average: {0:F2}", elements.Average());
+            Console.WriteLine();
         }
 
-       
+
+
     }
 }
--- Object-Oriented-Programming/03. Extension-Delegates-Lambda-LINQ/2. IEnumerable extensions/GenericExtension.cs
+++ Object-Oriented-Programming/03. Extension-Delegates-Lambda-LINQ/2. IEnumerable extensions/GenericExtension.cs
@@ -17,6 +17,35 @@ namespace IEnumerable_extensions
                 sum += (dynamic)item;
             }
             return sum;
-        }        
+        }
+        public static T MultiplyElement<T>(this IEnumerable<T> collection)
+        {
+            T mult = (dynamic)1;
+            foreach (var item in collection)
+            {
+                mult *= (dynamic)item;
+            }
+
+            return mult;
+        }
+           public static T MinElement<T>(this IEnumerable<T> collection)
+        {
+            return collection.Min<T>();
+        }
+        public static T MaxElement<T>(this IEnumerable<T> collection)
+        {
+            return collection.Max<T>();
+        }
+        public static T Average<T>(this IEnumerable<T> collection)
+        {
+            T avrg = default(T);
+            foreach (var item in collection)
+            {
+                avrg += (dynamic)item; 
+            }
+            int length = collection.Count();
+            avrg /= (dynamic)length;
+            return avrg;
+        }
     }
 }
