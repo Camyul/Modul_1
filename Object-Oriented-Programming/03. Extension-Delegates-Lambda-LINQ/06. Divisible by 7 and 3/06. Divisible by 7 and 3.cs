ee5f4bcb921cad261e5810629b4e5e1aec740aeb
Camyul <vankata.lt@outlook.com>
2016-12-27 11:37:37 +0200

    Complete CSharp Task 03.06

---
+++ Object-Oriented-Programming/03. Extension-Delegates-Lambda-LINQ/06. Divisible by 7 and 3/06. Divisible by 7 and 3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Divisible_by_7_and_3
+{
+    class DivisibleBySevenAndThree
+    {
+        static void Main()
+        {
+            int[] arrIntegers = new int[] {1, 12, 14, 23, 43, 21, 44, 28, 27, 87, 56 };
+            int[] orderedArr = Array.FindAll(arrIntegers, num => num % 3 == 0 || num % 7 == 0);
+            Console.WriteLine(String.Join(" ", orderedArr.ToArray()));
+        }
+    }
+}
