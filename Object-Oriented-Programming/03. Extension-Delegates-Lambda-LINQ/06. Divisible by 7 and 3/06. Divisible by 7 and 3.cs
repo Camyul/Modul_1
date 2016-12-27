507ad18d56ae7573d2e1fbaf375b1a63d549af99
Camyul <vankata.lt@outlook.com>
2016-12-27 11:53:56 +0200

    Complete CSharp Task 03.06 - add LINQ method

--- Object-Oriented-Programming/03. Extension-Delegates-Lambda-LINQ/06. Divisible by 7 and 3/06. Divisible by 7 and 3.cs
+++ Object-Oriented-Programming/03. Extension-Delegates-Lambda-LINQ/06. Divisible by 7 and 3/06. Divisible by 7 and 3.cs
@@ -11,8 +11,16 @@ namespace _06.Divisible_by_7_and_3
         static void Main()
         {
             int[] arrIntegers = new int[] {1, 12, 14, 23, 43, 21, 44, 28, 27, 87, 56 };
-            int[] orderedArr = Array.FindAll(arrIntegers, num => num % 3 == 0 || num % 7 == 0);
+            int[] orderedArr = Array.FindAll(arrIntegers, num => num % 3 == 0 || num % 7 == 0); // Using extension methods and lambda expressions
+            Console.WriteLine("Using extension methods and lambda expressions");
             Console.WriteLine(String.Join(" ", orderedArr.ToArray()));
+            Console.WriteLine();
+            Console.WriteLine("Using LINQ");                                                    //Using LINQ
+            var secondOrderedArr = from num in arrIntegers
+                                     where num % 3 == 0 || num % 7 == 0
+                                     select num;
+            Console.WriteLine(String.Join(" ", secondOrderedArr.ToArray()));
+
         }
     }
 }
