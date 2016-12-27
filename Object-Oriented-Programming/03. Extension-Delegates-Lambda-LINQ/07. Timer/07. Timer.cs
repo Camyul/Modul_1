e39c4726df5b7aebca77da064a51b1e3a7ecb3e5
Camyul <vankata.lt@outlook.com>
2016-12-27 21:52:26 +0200

    Complete CSharp Task 03.07

---
+++ Object-Oriented-Programming/03. Extension-Delegates-Lambda-LINQ/07. Timer/07. Timer.cs
@@ -0,0 +1,28 @@
+﻿namespace _07.Timer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    
+    public class Timer_Task7
+    {
+        static void Main()
+        {
+            Timer start = new Timer(3);
+            start.DlgAddMethod += FirstMethod;
+            start.DlgAddMethod += SecondMethod;
+            start.Execute();
+        }
+        private static void FirstMethod()
+        {
+            Console.WriteLine("I am a first Method");
+        }
+        private static void SecondMethod()
+        {
+            Console.WriteLine("I am a second Method");
+        }
+    }
+}
---
+++ Object-Oriented-Programming/03. Extension-Delegates-Lambda-LINQ/07. Timer/Timer.cs
@@ -0,0 +1,47 @@
+﻿
+
+namespace _07.Timer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    public class Timer
+    {
+        public delegate void TimerDelegate();
+
+        private int timeInterval;
+
+        public Timer(int interval)
+        {
+            this.TimeInterval = interval;
+        }
+
+        public int TimeInterval
+        {
+            get {return this.timeInterval; }
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("Interval must be > 0");
+                }
+                this.timeInterval = value;
+            }
+        }
+        public TimerDelegate DlgAddMethod { get; set; }
+
+        public void Execute()
+        {
+            
+            while (true)
+            {
+                this.DlgAddMethod();
+                Thread.Sleep(this.timeInterval * 1000);
+            }
+        }
+
+    }
+}
