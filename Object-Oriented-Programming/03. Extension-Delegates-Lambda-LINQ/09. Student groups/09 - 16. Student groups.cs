42bd3a2ab42ed167f2824680f20610fdddda70a2
Camyul <vankata.lt@outlook.com>
2016-12-29 21:41:43 +0200

    Complete CSharp Task 03.18

--- Object-Oriented-Programming/03. Extension-Delegates-Lambda-LINQ/09. Student groups/09 - 16. Student groups.cs
+++ Object-Oriented-Programming/03. Extension-Delegates-Lambda-LINQ/09. Student groups/09 - 16. Student groups.cs
@@ -1,26 +1,27 @@
 ﻿namespace Student_groups
 {
+    using ClassLibrary;
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using ClassLibrary;
-    using System.Text;
-    using System.Threading.Tasks;
     public class StudentGroups
     {
+        //Create list of Students
+        public static List<Student> listOfStudent = new List<Student>   //Create List Of Student - Task 9
+        {
+            new Student("Ivan", "Penev", "0220114565", "02501560", "test1@abv.bg", 3), //Add Student
+            new Student("Genka", "Veleva", "0120126954", "0878945225", "test2@mail.bg", 2),
+            new Student("Mitko", "Zanev", "1220063245", "056201205", "test3@abv.bg", 2),
+            new Student("Petar", "Lalev", "0420126565", "02444555", "test4@gmail.com", 1),
+            new Student("Maria", "Dobreva", "0120014532", "0887256321", "test5@abv.bg", 2),
+            new Student("Tanya", "Koleva", "0720102235", "064205322", "test6@mail.ru", 3),
+            new Student("Tanya", "Toteva", "0420158741", "0877212001", "test7@yahoo.com", 2),
+            new Student("Zoran", "Chernev", "0820063598", "02301888", "test8@mail.bg", 1),
+            new Student("Sindy", "Mileva", "0920082182", "032547124", "test9@abv.bg", 2)
+        };
         static void Main()
         {
-            //Create list of Students
-            List<Student> listOfStudent = new List<Student>();   //Create List Of Student
-            listOfStudent.Add(new Student("Ivan", "Penev", "0220114565", "02501560", "test1@abv.bg", 3)); //Add Student
-            listOfStudent.Add(new Student("Genka", "Veleva", "0120126954", "0878945225", "test2@mail.bg", 2));
-            listOfStudent.Add(new Student("Mitko", "Zanev", "1220063245", "056201205", "test3@abv.bg", 2));
-            listOfStudent.Add(new Student("Petar", "Lalev", "0420126565", "02444555", "test4@gmail.com", 1));
-            listOfStudent.Add(new Student("Maria", "Dobreva", "0120014532", "0887256321", "test5@abv.bg", 2));
-            listOfStudent.Add(new Student("Tanya", "Koleva", "0720102235", "064205322", "test6@mail.ru", 3));
-            listOfStudent.Add(new Student("Tanya", "Toteva", "0420158741", "0877212001", "test7@yahoo.com", 2));
-            listOfStudent.Add(new Student("Zoran", "Chernev", "0820063598", "02301888", "test8@mail.bg", 1));
-            listOfStudent.Add(new Student("Sindy", "Mileva", "0920082182", "032547124", "test9@abv.bg", 2));
+            
             //Add List of Marks for every student with loop and random generator
             Random rnd = new Random();
             foreach (var st in listOfStudent)       // Generate Random Marks
---
+++ Object-Oriented-Programming/03. Extension-Delegates-Lambda-LINQ/18 - 19. Grouped by GroupNumber/18. Grouped by GroupNumber.cs
@@ -0,0 +1,34 @@
+﻿namespace Grouped_by_GroupNumber
+{
+    using System;
+    using Student_groups;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using ClassLibrary;
+    class GroupedByGroupNumber
+    {
+        static void Main()
+        {
+            Console.WriteLine("Extract all students grouped by GroupNumber and prints to the console. - Task 18");
+            Console.WriteLine(new String('-', 80));
+            var groupedStudents = from st in StudentGroups.listOfStudent
+                                  group st by st.GroupNum
+                                  into groups
+                                  select new
+                                  {
+                                      Group = groups.Key,
+                                      Students = groups
+                                  };
+            foreach (var gr in groupedStudents)
+            {
+                Console.Write("\nGroup: {0} Students:\n--------------\n{1}", gr.Group,
+                                                    string.Join("\r\n", gr.Students));
+                Console.WriteLine("\r\n" + new string('-', 65));
+            }
+
+
+        }
+    }
+}
