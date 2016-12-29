4c3eeb1c7fa18eff4cc880c2a43aa5200672a729
Camyul <vankata.lt@outlook.com>
2016-12-29 11:23:24 +0200

    Complete CSharp Task 03.16

--- Object-Oriented-Programming/03. Extension-Delegates-Lambda-LINQ/09. Student groups/09 - 16. Student groups.cs
+++ Object-Oriented-Programming/03. Extension-Delegates-Lambda-LINQ/09. Student groups/09 - 16. Student groups.cs
@@ -12,17 +12,17 @@
         {
             //Create list of Students
             List<Student> listOfStudent = new List<Student>();   //Create List Of Student
-            listOfStudent.Add(new Student("Ivan",  "Penev",   "0220114565", "02501560",   "test1@abv.bg",      1)); //Add Student
-            listOfStudent.Add(new Student("Genka", "Veleva",  "0120126954", "0878945225", "test2@mail.bg",     2));
-            listOfStudent.Add(new Student("Mitko", "Zanev",   "1220073245", "056201205",  "test3@abv.bg",      2));
-            listOfStudent.Add(new Student("Petar", "Lalev",   "0420126565", "02444555",   "test4@gmail.com",   1));
-            listOfStudent.Add(new Student("Maria", "Dobreva", "0120014532", "0887256321", "test5@abv.bg",      2));
-            listOfStudent.Add(new Student("Tanya", "Koleva",  "0720102235", "064205322",  "test6@mail.ru",     1));
-            listOfStudent.Add(new Student("Tanya", "Toteva",  "0420158741", "0877212001", "test7@yahoo.com",   2));
-            listOfStudent.Add(new Student("Zoran", "Chernev", "0820063598", "02301888",   "test8@mail.bg",     1));
-            listOfStudent.Add(new Student("Sindy", "Mileva",  "0920082182", "032547124",  "test9@abv.bg",      2));
+            listOfStudent.Add(new Student("Ivan", "Penev", "0220114565", "02501560", "test1@abv.bg", 3)); //Add Student
+            listOfStudent.Add(new Student("Genka", "Veleva", "0120126954", "0878945225", "test2@mail.bg", 2));
+            listOfStudent.Add(new Student("Mitko", "Zanev", "1220063245", "056201205", "test3@abv.bg", 2));
+            listOfStudent.Add(new Student("Petar", "Lalev", "0420126565", "02444555", "test4@gmail.com", 1));
+            listOfStudent.Add(new Student("Maria", "Dobreva", "0120014532", "0887256321", "test5@abv.bg", 2));
+            listOfStudent.Add(new Student("Tanya", "Koleva", "0720102235", "064205322", "test6@mail.ru", 3));
+            listOfStudent.Add(new Student("Tanya", "Toteva", "0420158741", "0877212001", "test7@yahoo.com", 2));
+            listOfStudent.Add(new Student("Zoran", "Chernev", "0820063598", "02301888", "test8@mail.bg", 1));
+            listOfStudent.Add(new Student("Sindy", "Mileva", "0920082182", "032547124", "test9@abv.bg", 2));
             //Add List of Marks for every student with loop and random generator
-            Random rnd = new Random();   
+            Random rnd = new Random();
             foreach (var st in listOfStudent)       // Generate Random Marks
             {
                 for (int i = 0; i < 3; i++)
@@ -87,10 +87,9 @@
                 Console.WriteLine(String.Join(" ", st.Marks));
             }
 
-            //Write down a similar program that extracts the students with exactly two marks "2"
+            //Write down a similar program that extracts the students with exactly two marks "2" - Task 14
             Console.WriteLine(new string('-', 65));
             Console.WriteLine("Write down a similar program that extracts the students with exactly two marks \"2\" -Task 14\n");
-           
             List<Student> stTwoTwo = listOfStudent.SearchTwoMarksTwo();
             if (stTwoTwo.Count < 1)
             {
@@ -99,9 +98,42 @@
                 Console.WriteLine("Please start the program again (CTRL + F5), if needed 4-5 times. \n");
                 Console.ForegroundColor = ConsoleColor.Gray;
             }
-            foreach (var st in stTwoTwo)
+            PrintWithMarks(stTwoTwo);
+
+            //Extract all Marks of the students that enrolled in 2006. - Task 15
+            Console.WriteLine(new string('-', 65));
+            Console.WriteLine("Extract all Marks of the students that enrolled in 2006. - Task 15\n");
+            var studentsFromYear = listOfStudent.Where(s => s.FacNum.IndexOf("06", 4, 2) > 0).ToList();
+            PrintWithMarks(studentsFromYear);
+
+            //Create a class Group with properties GroupNumber and DepartmentName. - Task 16
+            Group group1 = new Group(1, "Mathematics");
+            Group group2 = new Group(2, "Sports");
+            Group group3 = new Group(3, "Mechanics");
+            List<Group> groups = new List<Group> { group1, group2, group3 };
+            //var studMathematics = listOfStudent.Where(st => st.GroupNum == 1).ToList();
+            var studMathematics = from gr in groups
+                                  where gr.GroupNum == 1
+                                  join st in listOfStudent on gr.GroupNum equals st.GroupNum
+                                  select new    // creating new anonymous class after matching group numbers from the Student class
+                                  {             // and the Group class to get Name from the Student instances and department from the Group ones
+                                      Name = st.FirstName + " " + st.LastName,
+                                      Department = group1.GroupName
+                                  };
+            Console.WriteLine(new string('-', 65));
+            Console.WriteLine("Mathematics department, extracted as new anonymous classes - Task 16*\n");
+            foreach (var st in studMathematics)
             {
-                Console.WriteLine(st.FirstName + " " + st.LastName + "\tfac.N: " + st.FacNum + "\tTel: " +st.Telefon);
+                Console.WriteLine(st);
+
+            }
+        }
+
+        private static void PrintWithMarks(List<Student> list)
+        {
+            foreach (var st in list)
+            {
+                Console.WriteLine(st.FirstName + " " + st.LastName + "\tfac.N: " + st.FacNum + "\tTel: " + st.Telefon);
                 Console.WriteLine("Marks: " + String.Join(" ", st.Mark));
             }
         }
---
+++ Object-Oriented-Programming/03. Extension-Delegates-Lambda-LINQ/ClassLibrary/Group.cs
@@ -0,0 +1,19 @@
+﻿namespace ClassLibrary
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class Group                      //Task 16
+    {
+        public int GroupNum { get; set; }  
+        public string GroupName { get; set; }
+
+        public Group(int number, string name)
+        {
+            this.GroupNum = number;
+            this.GroupName = name;
+        }
+    }
+}
