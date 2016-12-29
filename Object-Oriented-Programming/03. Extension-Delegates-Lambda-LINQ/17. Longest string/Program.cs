b0f0efd1a6a10ea1f7def8ed7c40ca7a3a841dd1
Camyul <vankata.lt@outlook.com>
2016-12-29 20:30:54 +0200

    Complete CSharp Task 03.17

---
+++ Object-Oriented-Programming/03. Extension-Delegates-Lambda-LINQ/17. Longest string/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Longest_string
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    class LongestString
+    {
+        static void Main() 
+        {
+            //Write a program to return the string with maximum length from an array of strings. - Task 17
+            Console.WriteLine("String with maximum length from an array of strings. - Task 17\n");
+            string[] someString = {
+                "Lorem Ipsum is simply dummy text of the printing",
+                " and typesetting industry. ",
+                "Lorem Ipsum has been the ",
+                "industry's standard dummy text ever since the 1500s, ",
+                "when an unknown printer took a galley of type and scrambled it to make a ",
+                "type specimen book. ",
+                "It has survived not only five centuries, but also the"
+            };
+            var longestString = from str in someString
+                                orderby str.Length descending
+                                select str;
+            Console.WriteLine("Longest string is:\n{0}", longestString.FirstOrDefault());
+        }
+    }
+}
