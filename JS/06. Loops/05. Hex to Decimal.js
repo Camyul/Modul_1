285caf78a8cab5d06b057d805e81066308440344
Camyul <vankata.lt@outlook.com>
2017-01-01 03:55:20 +0200

    Complete JS Task 06.05

--- JS/06. Loops/05. Hex to Decimal.js
+++ JS/06. Loops/05. Hex to Decimal.js
@@ -1,9 +1,8 @@
 function solve(args) {
-    'use strict';
     let hexNum = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'],
         result = 0,
-        l = args.length,
-        str = args[0].toUpperCase();
+        str = args.toString(),
+        l = str.length;
     for (let i = 0; i < l; i += 1) {
         let digit = str[l - i - 1];
         let convSymbol = hexNum.indexOf(digit);
