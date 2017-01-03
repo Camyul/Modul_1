d3dfe922a5070f9cff00ba005cf056174e1855ef
Camyul <vankata.lt@outlook.com>
2017-01-03 19:05:33 +0200

    Complete JS Task 07.07

--- JS/07. Arrays/debug.js
+++ JS/07. Arrays/debug.js
@@ -1,16 +1,19 @@
 function solve(args) {
     'use strict';
     let l,
-        start = 1,
+        start = 0,
         end,
         mid,
         element = +args.pop();
-    //args.splice(0, 1);
+    args.splice(0, 1);
     l = args.length;
     end = l;
     while (true) {
         mid = (((end - start) / 2) | 0) + start;
-        if (element > +args[mid]) {
+        if ((+args[end] > element) && (+args[end - 1] < element)) {
+            mid = -1;
+            break;
+        } else if (element > +args[mid]) {
             start = mid;
         } else if (element < +args[mid]) {
             end = mid;
@@ -18,6 +21,6 @@ function solve(args) {
             break;
         }
     }
-    console.log(mid - 1);
+    console.log(mid);
 }
 solve(['10', '1', '2', '4', '8', '14', '16', '31', '32', '64', '77', '99', '32']);
