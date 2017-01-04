c2a232f95f339f3b93c21a0b5c597c2951f91f62
Camyul <vankata.lt@outlook.com>
2017-01-04 10:52:39 +0200

    Complete on JS Task 08.06

--- JS/08. Functions/06. First larger than neighbours.js
+++ JS/08. Functions/06. First larger than neighbours.js
@@ -2,14 +2,14 @@ function solve(args) {
     'use strict';
     let arr = args[1].split(' '),
         l = arr.length,
-        count = 0;
+        i;
 
-    for (let i = 1; i < l - 1; i += 1) {
+    for (i = 1; i < l - 1; i += 1) {
         if (largestThan(i, arr[i])) {
-            count++;
+            break;
         }
     }
-    console.log(count);
+    console.log(i);
 
     function largestThan(j, element) {
         if ((+element > +arr[j - 1]) && (+element > +arr[j + 1])) {
