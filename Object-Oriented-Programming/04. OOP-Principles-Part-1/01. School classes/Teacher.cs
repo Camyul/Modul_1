dbba319b03b50f7bd0e13ac436d5b3d8eeac7da2
Camyul <vankata.lt@outlook.com>
2017-01-06 22:31:25 +0200

    Add class Teacher CSharp Task 04.01

---
+++ Object-Oriented-Programming/04. OOP-Principles-Part-1/01. School classes/Teacher.cs
@@ -0,0 +1,27 @@
+﻿namespace School_classes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class Teacher : Person, IComment
+    {
+        private List<string> textBlock = new List<string>(); //For IComment
+
+        public Teacher(string first, string last) : base(first, last) //Constructor
+        {
+
+        }
+
+        public void AddComent(string comment) //For IComment
+        {
+            textBlock.Add(comment);
+        }
+
+        public override string ToString()  //Override method ToString()
+        {
+            return String.Format("{0} {1} {2}\n{3}", this.FirstName, this.LastName, this.StudentID, string.Join("\n", textBlock));
+        }
+    }
+}
