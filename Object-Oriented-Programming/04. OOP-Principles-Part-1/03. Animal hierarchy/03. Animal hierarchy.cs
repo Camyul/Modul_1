c7d1bbbef209711f9cc184a68ca3439eadec12a5
Camyul <vankata.lt@outlook.com>
2017-01-07 20:00:26 +0200

    Complete CSharp ISound Task 04.03

---
+++ Object-Oriented-Programming/04. OOP-Principles-Part-1/03. Animal hierarchy/03. Animal hierarchy.cs
@@ -0,0 +1,15 @@
+﻿namespace Animal_hierarchy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    class AnimalHierarchy
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
---
+++ Object-Oriented-Programming/04. OOP-Principles-Part-1/03. Animal hierarchy/Animals.cs
@@ -0,0 +1,35 @@
+﻿namespace Animal_hierarchy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class Animals
+    {
+        private string name;
+        private Gender gender;
+        private int age;
+
+        public Animals(string name, Gender gender, int age)
+        {
+            this.Name = name;
+            this.Gender = gender;
+            this.Age = age; 
+        }
+
+        public string Name { get; private set; }
+        public Gender Gender { get; private set; }
+        public int Age {
+            get { return this.age; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Age cannot be negative");
+                }
+                this.age = value;
+            }
+        }
+    }
+}
