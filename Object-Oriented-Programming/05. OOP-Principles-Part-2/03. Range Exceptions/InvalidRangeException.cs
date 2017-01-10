8bc302deaf9cf51a0b5c6d42e636f7ff8b100c19
Camyul <vankata.lt@outlook.com>
2017-01-10 10:31:34 +0200

    Add Class Diagram CSharp Task 05.02

--- Object-Oriented-Programming/05. OOP-Principles-Part-2/03. Range Exceptions/InvalidRangeException.cs
+++ Object-Oriented-Programming/05. OOP-Principles-Part-2/03. Range Exceptions/InvalidRangeException.cs
@@ -1,10 +1,6 @@
 ﻿namespace Range_Exceptions
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     public class InvalidRangeException<T> : ArgumentOutOfRangeException
     {
         public InvalidRangeException(string msg, T Start, T End) : base(msg)
