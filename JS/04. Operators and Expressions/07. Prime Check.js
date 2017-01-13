23b0f030377dabcbbc924d468eafe74fd1e911fd
Camyul <vankata.lt@outlook.com>
2017-01-13 14:58:33 +0200

    Correct JS Task 04.07

--- JS/04. Operators and Expressions/07. Prime Check.js
+++ JS/04. Operators and Expressions/07. Prime Check.js
@@ -1,20 +1,19 @@
-function solve(args){
-var num = 17;
-var sqrtNum = Math.floor(Math.sqrt(num));
-var isPrime = true;
-if(num > 1){
-    for(var i = 2; i <= sqrtNum; i+=1) {
-        if ((num % i) != 0) {
-            continue;
-        } else {
-            isPrime = false;
-            return isPrime; 
+function solve(args) {
+    var num = args[0];
+    var sqrtNum = Math.floor(Math.sqrt(num));
+    var isPrime = true;
+    if (num > 1) {
+        for (var i = 2; i <= sqrtNum; i += 1) {
+            if ((num % i) != 0) {
+                continue;
+            } else {
+                isPrime = false;
+                return isPrime;
+            }
         }
+        return isPrime;
+    } else {
+        isPrime = false;
+        return isPrime;
     }
-    return isPrime;
-}
-else{
-    isPrime = false;
-    return isPrime;
-}
 }
