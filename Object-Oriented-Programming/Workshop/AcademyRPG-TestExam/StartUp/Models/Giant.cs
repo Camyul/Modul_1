42cf22842f0a375757738966ea4b944aa292cf1b
Camyul <vankata.lt@outlook.com>
2017-01-14 22:04:22 +0200

    Test Exam

---
+++ Object-Oriented-Programming/Workshop/AcademyRPG-TestExam/StartUp/Models/Giant.cs
@@ -0,0 +1,77 @@
+﻿namespace AcademyRPG
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class Giant : Character, IFighter, IGatherer
+    {
+        private bool increaseAttack;
+
+        public Giant(string name, Point position) : base(name, position, 0)
+        {
+            this.HitPoints = 200;
+            this.increaseAttack = false;
+        }
+
+        public bool IncreaseAttack
+        {
+            get { return this.increaseAttack; }
+            private set
+            {
+                if (value == true)
+                {
+                    this.increaseAttack = value;
+                }
+            }
+        }
+        public int AttackPoints
+        {
+            get
+            {
+                if (increaseAttack)
+                {
+                    return 250;
+                }
+                return 150;
+            }
+        }
+
+        public int DefensePoints
+        {
+            get
+            {
+                return 80;
+            }
+        }
+
+        
+
+        public int GetTargetIndex(List<WorldObject> availableTargets)
+        {
+            for (int i = 0; i < availableTargets.Count; i++)
+            {
+                if (availableTargets[i].Owner != 0)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public bool TryGather(IResource resource)
+        {
+            if (resource.Type == ResourceType.Stone)
+            {
+              
+                this.IncreaseAttack = true;
+                
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Object-Oriented-Programming/Workshop/AcademyRPG-TestExam/StartUp/Models/Ninja.cs
@@ -0,0 +1,75 @@
+﻿namespace AcademyRPG
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class Ninja : Character, IFighter, IGatherer
+    {
+        private int attackPoints;
+        private int hitPoints;
+        public Ninja(string name, Point position, int owner) : base(name, position, owner)
+        {
+            this.HitPoints = 1;
+            this.AttackPoints = 0;
+        }
+
+        public new int HitPoints
+        {
+            get { return this.hitPoints; }
+            private set { 
+            //{
+            //    if (value <= 0)
+            //    {
+            //        this.hitPoints = 1;
+            //    }
+                this.hitPoints = value;
+            }
+        }
+        public int AttackPoints
+        {
+            get { return this.attackPoints;}
+            private set { this.attackPoints = value; }
+        }
+
+        public int DefensePoints
+        {
+            get
+            {
+                return int.MaxValue; 
+            }
+        }
+
+        
+
+        public int GetTargetIndex(List<WorldObject> availableTargets)
+        {
+            var target = availableTargets
+                 .Where(x => x.Owner != 0 && x.Owner != this.Owner)
+                 .OrderByDescending(x => x.HitPoints)
+                 .FirstOrDefault();
+
+
+            return availableTargets.IndexOf(target);
+        }
+
+        public bool TryGather(IResource resource)
+        {
+            if (resource.Type == ResourceType.Stone)
+            {
+                this.AttackPoints += resource.Quantity * 2;
+                return true;
+            }
+            else if (resource.Type == ResourceType.Lumber)
+            {
+                this.AttackPoints += resource.Quantity;
+                return true;
+            }
+
+            return false;
+        }
+
+        
+    }
+}
