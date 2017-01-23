a4ebdffd19d2670c90d89fca7b5eddf6fc3741a8
Camyul <vankata.lt@outlook.com>
2017-01-24 00:48:45 +0200

    Some corrextions

--- Object-Oriented-Programming/CSharp - exam/Telerik Academy_Skeleton (zip)/Academy/Commands/Listing/ListUsersCommand.cs
+++ Object-Oriented-Programming/CSharp - exam/Telerik Academy_Skeleton (zip)/Academy/Commands/Listing/ListUsersCommand.cs
@@ -5,12 +5,7 @@ namespace Academy.Commands.Listing
 {
     static class ListUsersCommand
     {
-        //public static IList<IStudent> OnsiteStudents(this IList<IStudent> collection) 
-        //{                                           
-            
-        //    collection.Add
-        //    return collection
-        //}
+        
 
     }
 }
